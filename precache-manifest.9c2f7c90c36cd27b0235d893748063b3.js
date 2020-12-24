52c9188d0af979654adc4ed7c2e491c291580128
Stuyk <stuykgaming@gmail.com>
2020-12-24 14:47:38 +0000

    Deploying to gh-pages from  @ af0b9efe83e2a307456279f99de2cbc4c6544f0b 🚀

---
+++ precache-manifest.9c2f7c90c36cd27b0235d893748063b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b47de2add8d8eccc16ff",
+    "url": "/css/chunk-vendors.7a67e2d8.css"
+  },
+  {
+    "revision": "48c8bd77bb466626a8090d3eab3139dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21c0d30501e297237fe6",
+    "url": "/js/app.bd96044b.js"
+  },
+  {
+    "revision": "b47de2add8d8eccc16ff",
+    "url": "/js/chunk-vendors.b78b0d00.js"
+  },
+  {
+    "revision": "708f8b4c86c21d38397b5de1968320c4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
