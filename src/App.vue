68786a10b611c71d63a07bb796d3ebeffe788f75
stuyk <stuykgaming@gmail.com>
2021-01-12 07:37:42 -0700

    Add Portless Options

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,95 @@
 <template>
-  <v-app dark>
-    <v-main>
-      <v-container class="main d-flex flex-column align-content-center align-center justify-center">
-        <template v-if="!loading">
-            <v-img class="mb-12" src="~@/assets/discord.svg" min-height="150" min-width="150" max-width="150" max-height="150" block></v-img>
-            <v-btn @mouseup="beginAuth">Click Here to Authorize with Discord</v-btn>
-            <p class="mt-6">A page will open up outside of your game and assist you with logging in.</p>
-        </template>
-        <template v-else>
-            <div class="lds-ring mb-12"><div></div><div></div><div></div><div></div></div>
-            <p>Tab out and check your browser to finish authentication. <br />If it failed try opening the window again. <a @mousedown="authAgain">Re-open Window.</a></p>
-        </template>
-        <p class="context mt-6">Ares Discord Authentication - Written by <a @click="loadAthena">Stuyk</a></p>
-      </v-container>
-    </v-main>
-  </v-app>
+    <v-app dark>
+        <v-main>
+            <v-container class="main d-flex flex-column align-content-center align-center justify-center fadein">
+                <template v-if="!loading">
+                    <v-img
+                        class="mb-12"
+                        src="~@/assets/discord.svg"
+                        min-height="150"
+                        min-width="150"
+                        max-width="150"
+                        max-height="150"
+                        block
+                    ></v-img>
+                    <v-btn @mouseup="beginAuth">Click Here to Authorize with Discord</v-btn>
+                    <p class="mt-6">A page will open up outside of your game and assist you with logging in.</p>
+                </template>
+                <template v-else>
+                    <div class="fadein d-flex flex-column align-content-center align-center justify-center">
+                        <template v-if="!isPortless">
+                            <div class="lds-ring mb-12">
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                            <p>
+                                Tab out and check your browser to finish authentication. <br />If it failed try opening
+                                the window again. <a @mousedown="authAgain">Re-open Window.</a>
+                            </p>
+                        </template>
+                        <template v-else>
+                            <p>
+                                Tab out and check your browser to finish authentication. <br />If it failed try opening
+                                the window again. <a @mousedown="authAgain">Re-open Window.</a>
+                            </p>
+                            <span class="text-sm-subtitle-2 pt-1 pb-5">
+                                Note: If pressed before authorization complete. Nothing will happen.
+                            </span>
+                            <v-btn @mouseup="finishAuth">Finish Authorization</v-btn>
+                        </template>
+                    </div>
+                </template>
+                <p class="context mt-6">Ares Discord Authentication - Written by <a @click="loadAthena">Stuyk</a></p>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
 
 <style>
-  body, html {
+body,
+html {
     overflow: hidden !important;
-  }
+}
 
-  .v-application {
+.v-application {
     background: url('~@/assets/bg.png') no-repeat center center fixed !important;
     background-size: stretch;
     background-repeat: no-repeat;
-  }
+}
 
-  .main {
+.main {
     width: 100vw;
     height: 100vh;
-  }
+}
 
-  .context {
+.context {
     position: fixed;
     bottom: 5vh;
-  }
+}
 
-  .lds-ring {
-  display: inline-block;
-  position: relative;
-  width: 150px;
-  height: 150px;
-  }
+.fadein {
+    animation: fadein 2s;
+}
 
-  .lds-ring div {
+@keyframes fadein {
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+}
+
+.lds-ring {
+    display: inline-block;
+    position: relative;
+    width: 150px;
+    height: 150px;
+}
+
+.lds-ring div {
     box-sizing: border-box;
     display: block;
     position: absolute;
@@ -56,100 +100,109 @@
     border-radius: 50%;
     animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
     border-color: #fff transparent transparent transparent;
-  }
+}
 
-  .lds-ring div:nth-child(1) {
+.lds-ring div:nth-child(1) {
     animation-delay: -0.45s;
-  }
+}
 
-  .lds-ring div:nth-child(2) {
+.lds-ring div:nth-child(2) {
     animation-delay: -0.3s;
-  }
+}
 
-  .lds-ring div:nth-child(3) {
+.lds-ring div:nth-child(3) {
     animation-delay: -0.15s;
-  }
+}
 
-  @keyframes lds-ring {
+@keyframes lds-ring {
     0% {
-      transform: rotate(0deg);
+        transform: rotate(0deg);
     }
     100% {
-      transform: rotate(360deg);
+        transform: rotate(360deg);
     }
-  }
+}
 </style>
 
 <script>
 export default {
-  name: 'App',
-  data() {
-      return {
-          url: null,
-          loading: false,
-          done: false,
-          updates: 0,
-      };
-  },
-  methods: {
-      setAsReady() {
-        this.$nextTick(() => {
-            this.updates += 1;
-        });
-      },
-      beginAuth() {
-          setTimeout(() => {
-              this.getURL();
-              this.loading = true;
-              this.updates += 1;
-          }, 100);
-      },
-      authAgain() {
-          this.getURL();
-      },
-      getURL() {
-          if ('alt' in window) {
-              alt.emit('discord:OpenURL');
-          }
-      },
-      openURL(url) {
-          if (this.window) {
-            try {
-              this.window.close();
-            } catch(err) {
-              console.log(err);
+    name: 'App',
+    data() {
+        return {
+            url: null,
+            loading: false,
+            isPortless: false,
+            done: false,
+            updates: 0
+        };
+    },
+    methods: {
+        setAsReady() {
+            this.$nextTick(() => {
+                this.updates += 1;
+            });
+        },
+        beginAuth() {
+            setTimeout(() => {
+                this.getURL();
+                this.loading = true;
+                this.updates += 1;
+            }, 100);
+        },
+        finishAuth() {
+            if (!this.isPortless) {
+                return;
             }
-          }
 
-          this.window = window.open(url);
-      },
-      loadAthena() {
-          window.open(`https://github.com/stuyk/altv-athena`);
-      },
-      finishedLoading() {
-          this.$nextTick(() => {
-              this.setAsReady();
-          });
-      },
-      endWindow() {
-          if (this.window) {
-            try {
-              this.window.close();
-            } catch(err) {
-              console.log(err);
+            if ('alt' in window) {
+                alt.emit('discord:FinishAuth');
             }
-          }
+        },
+        authAgain() {
+            this.getURL();
+        },
+        getURL() {
+            if ('alt' in window) {
+                alt.emit('discord:OpenURL');
+            }
+        },
+        openURL(url, isPortless = false) {
+            if (this.window) {
+                try {
+                    this.window.close();
+                } catch (err) {
+                    console.log(err);
+                }
+            }
 
-      }
-  },
-  mounted() {
-    if ('alt' in window) {
-        alt.on('discord:OpenURL', this.openURL);
-        alt.on('discord:endWindow', this.endWindow);
+            this.isPortless = isPortless;
+            this.window = window.open(url);
+        },
+        loadAthena() {
+            window.open(`https://github.com/stuyk/altv-athena`);
+        },
+        finishedLoading() {
+            this.$nextTick(() => {
+                this.setAsReady();
+            });
+        },
+        endWindow() {
+            if (this.window) {
+                try {
+                    this.window.close();
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+        }
+    },
+    mounted() {
+        if ('alt' in window) {
+            alt.on('discord:OpenURL', this.openURL);
+            alt.on('discord:endWindow', this.endWindow);
+        }
+
+        this.finishedLoading();
     }
-
-    this.finishedLoading();
-  }
 };
 </script>
-
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  vuetify,
-  mounted() {
-    vuetify.framework.theme.dark = true;
-  },
-  render: function (h) { return h(App) }
-}).$mount('#app')
+    vuetify,
+    mounted() {
+        vuetify.framework.theme.dark = true;
+    },
+    render: function(h) {
+        return h(App);
+    }
+}).$mount('#app');
